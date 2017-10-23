1b644bce6c5673799a8777263dbffc793bea17ae
Lucassa3 <lucassa3@al.insper.edu.br>
2017-10-23 09:20:51 -0200

    fixed All components and Alu integration

--- MIPS/Alu.vhd
+++ MIPS/Alu.vhd
@@ -1,36 +1,33 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+
 entity Alu is
+	port( 
 
-port( 
-
-	in_a, in_b	:	in std_logic_vector(31 downto 0);
-	ctrl			:	in std_logic_vector(3 downto 0);
-	output		:	out std_logic_vector(31 downto 0)
-	
+		in_a, in_b	:	in std_logic_vector(31 downto 0);
+		ctrl			:	in std_logic_vector(3 downto 0);
+		output		:	out std_logic_vector(31 downto 0)
+		
 	);
-	
 end entity;
+
 
 architecture bhv of Alu is
 
-signal mux_a_out, mux_b_out, fa_out, and_out, or_out, slt_out : std_logic_vector(31 downto 0);
+	signal mux_a_out, mux_b_out, fa_sum, and_out, or_out, slt_out : std_logic_vector(31 downto 0);
+	signal fa_carry : std_logic;
 
 begin
 
-	process(in_a, in_b, ctrl)
-	begin
-	
 	MUX_A : entity work.Mux port map (in_a, not(in_a), ctrl(3), mux_a_out);
 	MUX_B : entity work.Mux port map (in_b, not(in_b), ctrl(2), mux_b_out);
-	FULL_ADDER : entity work.FullAdder32 port map (mux_a_out, mux_b_out, ctrl(2), fa_out);
+	FULL_ADDER : entity work.FullAdder32 port map (mux_a_out, mux_b_out, ctrl(2), fa_sum, fa_carry);
 	
 	or_out <= mux_a_out or mux_b_out;
 	and_out <= mux_a_out and mux_b_out;
-	slt_out <= '0'; --CHECK
+	slt_out <= "00000000000000000000000000000000"; --CHECK
 	
-	MUX_ALU_OUT : entity work.Mux4Way port map (slt_out, fa_out, or_out, and_out, ctrl(1 downto 0, output);
+	MUX_OPS : entity work.Mux4Way port map (slt_out, fa_sum, or_out, and_out, ctrl(1 downto 0), output);
 	
-	end process;
 end architecture;
--- MIPS/Mux.vhd
+++ MIPS/Mux.vhd
@@ -1,23 +1,26 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Mux is
 
+entity Mux is
 	port ( 
 		in_a, in_b : in std_logic_vector(31 downto 0);
 		sel  : in std_logic;
 		output : out std_logic_vector(31 downto 0)	
-	);
-	
+	);	
 end entity;
+
 
 architecture bhv of Mux is
 begin
+	
 	process(in_a, in_b, sel)
 	begin
+		
 		case sel is
 			when '0' => output <= in_a;
 			when '1' => output <= in_b;
-		end case; 
+		end case;
+		
 	end process;
 end architecture;
